44579d56b0b2174313b3eb02b84c7fb81f673b8a
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-09-11 14:54:17 +0500

    basic frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar
+                style={{ backgroundColor: 'rgba(33,33,33,0.95)' }}
+                variant='dark'
+                expand='lg'
+                collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <div>Dukaan</div>
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav style={{ 'margin-left': 'auto' }}>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <i className='fas fa-shopping-cart' /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <div>
+                                        <i className='fas fa-user' /> Sign In
+                                    </div>
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
